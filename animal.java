ec29946859956b22fe6ba4d340ec6334da56f7e0
Veronika <veranika.poliansckaya@yandex.ru>
2021-06-22 20:38:10 +0700

    version 3

---
+++ animal.java
@@ -0,0 +1,15 @@
+class Animals {
+    public static <String> void main(String[] args) {
+        String Dog ;
+        int age;
+        String colour;
+
+        void barking(){
+        }
+
+        void sleeping (){
+        }
+        void hungry(){
+
+        }
+    }
